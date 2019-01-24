b60461f40228b3b4d877902d95991282aae68bf8
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-24 02:32:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
-require "pry"
+
 
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
 
-  dwarves.to_enum.with_index(1).each do |value, index|
+  dwarves.with_index(1).each do |value, index|
     binding.pry
     puts "#{index}. #{value}"
 end
